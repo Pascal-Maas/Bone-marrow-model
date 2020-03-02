ac4849c25d6b9ee886e3e1ed2247f07797c09753
Pascal <p.maas92@gmail.com>
2020-03-02 15:56:35 +0100

    Few scripts to get started
    Seurat.R describes the preprocessing from counts to PCA matrix

---
+++ Scripts/Exploration.R
@@ -0,0 +1,63 @@
+setwd("Data/")
+
+get_list_of_matrices <- function(){
+    data_files <- list.files()
+    return(lapply(data_files, function(file){
+        return(t(get(load(file))))
+    }))
+}
+
+l <- get_list_of_matrices()
+
+
+m1 <- get(load('SRA779509_SRS3805255.sparse.RData'))
+typeof(m1)
+
+m2 <- get(load('SRA779509_SRS3805247.sparse.RData'))
+dplyr::bind_rows(m1, m2)
+rbind2(m1, m2, fill=T)
+
+# Columns are cells (tags for mRNA)
+# Rows are genes
+
+names <- strsplit(rownames(m1), "_") 
+a <- unlist(lapply(names, function(s){ # get Only ENGSG00....
+    return(s[2])
+}))
+
+summary(m1[which(startsWith(a, "ENSG00000010610") == T),]) #CD4
+
+# TODO:
+# Transpose so cells are horizontal, genes are vertical
+# Determine the labels of each cell (by overexpression of CD4, CD8 etc..? Or maybe other way?)
+# Find a way to combine cells from different patients (averaging maybe? Concatenating?)
+# Explore which genes cause most variation (PCA)
+# Perform feature selection
+# Determine 
+# Make model
+# Combine into pipeline
+
+
+# dummy data
+set.seed(3344)
+A = Matrix(matrix(rbinom(16, 2, 0.2), 4))
+colnames(A)=letters[1:4]
+B = Matrix(matrix(rbinom(9, 2, 0.2), 3))
+colnames(B) = letters[3:5]
+
+# finding what's missing
+misA = colnames(B)[!colnames(B) %in% colnames(A)]
+misB = colnames(A)[!colnames(A) %in% colnames(B)]
+
+misAl = as.vector(numeric(length(misA)), "list")
+names(misAl) = misA
+misBl = as.vector(numeric(length(misB)), "list")
+names(misBl) = misB
+
+## adding missing columns to initial matrices
+An = do.call(cbind, c(A, misAl))
+Bn = do.call(cbind, c(B, misBl))[,colnames(An)]
+
+# final bind
+rbind(An, Bn)
+
---
+++ Scripts/Seurat.R
@@ -0,0 +1,82 @@
+library(dplyr)
+library(Seurat)
+
+# Load the PBMC dataset
+pbmc.data <- get(load("Data/SRA779509_SRS3805245.sparse.RData"))
+# Initialize the Seurat object with the raw (non-normalized data).
+pbmc <- CreateSeuratObject(counts = pbmc.data, project = "pbmc3k", min.cells = 3, min.features = 200)
+
+
+# Calculates the percentage of counts coming from Mitochondria genes (starting with MT), per cell
+pbmc[["percent.mt"]] <- PercentageFeatureSet(pbmc, pattern = "^MT-")
+
+# Shows the QC data when loading SeuratObject. 
+# nCount = total RNA molecules
+# nFeature = Number of unique RNA molecules
+# percent.mt = Percentage mitochondria (as previously calculated)
+head(pbmc@meta.data, 5)
+
+# Shows the vignette plot of these metdata. The red area shows the spread
+VlnPlot(pbmc, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3)
+
+
+# These plots show the relationship between the metadata using a scatterplot
+plot1 <- FeatureScatter(pbmc, feature1 = "nCount_RNA", feature2 = "percent.mt") # Weak negative correlation
+plot2 <- FeatureScatter(pbmc, feature1 = "nCount_RNA", feature2 = "nFeature_RNA") # Strong positive correlation
+CombinePlots(plots = list(plot1, plot2))
+
+# Create a subset of the original set. Only take:
+# cells with > 200 unique RNA molecules, (leaked out of broken membrane)
+# cells with < 2500 unique RNA molecules, done to filter doubles
+# cells with smaller than 5 percent mitochondria RNA (leaked out of broken membrane)
+pbmc <- subset(pbmc, subset = nFeature_RNA > 200 & nFeature_RNA < 2500 & percent.mt < 5) 
+
+# Scale the counts by 10.000, then normalize using a log transformation
+pbmc <- NormalizeData(pbmc, normalization.method = "LogNormalize", scale.factor = 10000)
+
+# Standardize the data by calculating the Z-score ((val - avg) / std dev). 
+# Outliers are clipped by sqrt(N), where N = amount of cells
+# Calculate the variance of each gene across all cells. This measure is done to controll for the mean expression.
+# Return the top 2.000 genes with the highest standardized variance
+pbmc <- FindVariableFeatures(pbmc, selection.method = "vst", nfeatures = 2000)
+
+
+# Identify the 10 most highly variable genes
+top10 <- head(VariableFeatures(pbmc), 10)
+
+
+# plot variable features with and without labels
+plot1 <- VariableFeaturePlot(pbmc)
+plot2 <- LabelPoints(plot = plot1, points = top10, repel = TRUE)
+plot(plot2)
+CombinePlots(plots = list(plot1, plot2))
+
+# Scale the data for PCA, but only select the genes that had a high variance.
+all.genes <- rownames(pbmc)
+pbmc <- ScaleData(pbmc, features = all.genes)
+
+# Performs a PCA on the scaled data. It will show the first 5 dimensions
+pbmc <- RunPCA(pbmc, features = VariableFeatures(pbmc))
+print(pbmc[["pca"]], dims = 1:5, nfeatures = 5)
+DimPlot(pbmc, reduction = "pca") # Plot a regular pca plot of the first 2 components
+VizDimLoadings(pbmc, dims = 1:2, reduction = "pca") # Show the loadings of the PCA. 
+DimHeatmap(pbmc, dims = 1, cells = 500, balanced = TRUE) # Heatmap of up versus down regulated
+
+# Permutation of Data, sampling 1% to create PCA's. 
+# Compares the PCA scores for the sampled genes to the observed PCA to calculate statistical significance 
+pbmc <- JackStraw(pbmc, num.replicate = 100, dims = 40)
+pbmc <- ScoreJackStraw(pbmc, dims = 1:40)
+
+# Get p-values per Principle Component
+pbmc[["pca"]]@jackstraw@overall.p.values
+
+# Plot the P-values
+JackStrawPlot(pbmc, dims = 15:40)
+
+# Alternatively, a Elbowplot can be made
+ElbowPlot(pbmc, ndims = 40)
+
+
+# Select PCA data for modelling:
+loadings <- pbmc[["pca"]]@feature.loadings # Gene loadings
+loadings[,1:12] # Replace '1:8' with 1:N' where N = number of PCs wanted.
